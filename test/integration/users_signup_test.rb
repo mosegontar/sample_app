a048ad771982bf1bc5ae69b8c059ee348d80389d
Alex Gontar <mosegontar@gmail.com>
2017-10-21 10:33:33 -0400

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
+
+    assert_select 'form#new_user[action="/signup"]'
+
     assert_no_difference 'User.count' do
       post users_path, params: {
         user: {
@@ -13,6 +16,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         }
       }
     end
+
     assert_template 'users/new'
+
+    assert_select 'div#error_explanation ul' do
+      assert_select "li", 4
+    end
+  end
+
+  test "valid signup information" do
+    get signup_path
+
+    assert_difference "User.count", 1 do
+      post users_path, params: {
+        user: {
+          name: "Frank",
+          email: "enstein@gmail.com",
+          password: "password",
+          password_confirmation: "password",
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
