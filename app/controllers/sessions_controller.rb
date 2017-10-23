4ec8e0ee2198763277327dbacf5773f2cc393dbd
Alex Gontar <mosegontar@gmail.com>
2017-10-22 20:48:29 -0400

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      # we use danger because alert-danger is
+      # a bootstrap class.
+      flash.now[:danger] = "Bad Login, Baby."
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out(current_user)
+    redirect_to root_url
+  end
+end
