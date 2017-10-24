1a239a500aa98a92cb1526b930e2dd8d98a639ff
Alex Gontar <mosegontar@gmail.com>
2017-10-24 13:54:32 -0400

    Add permanent cookie infrastructure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -12,9 +13,32 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  # Returns the hash digest of a given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user:
+  # sets remember_digest attribute to nil
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
